cc0b81aa97b031bab60e9698f469b0948c67919a
Kyle Anderson <andersonkylea324@gmail.com>
2019-09-26 23:01:28 -0700

    done

--- part2/TestCases.java
+++ part2/TestCases.java
@@ -41,7 +41,7 @@ public class TestCases
    @Test
    public void testGetAngle()
    {
-      assertEquals(.785, new Point(2.0, 2.0).getAngle(), DELTA);
+      assertEquals(.785, new Point(2.0, 2.0).getAngle(), .001);
    }
 
    @Test
